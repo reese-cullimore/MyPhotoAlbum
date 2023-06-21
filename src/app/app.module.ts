8dfe4a5f90f73403f750282e913564a10d2e9795
Reese Cullimore <reesecullimore@Reeses-MacBook-Pro-2.local>
2023-06-21 13:55:29 -0600

    pre-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AlbumComponent } from './album/album.component';
+
 import { IAlbumService, PlaceholderJSONAlbumService } from './album/album.service';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -26,16 +28,16 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatInputModule,
     MatIconModule,
-    ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatProgressBarModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatExpansionModule
+    MatExpansionModule,
   ],
   providers: [
     {provide: IAlbumService, useExisting: PlaceholderJSONAlbumService}
